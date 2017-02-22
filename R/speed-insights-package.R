c3149d9b8014378c945483d9ed8de220310c96dd
Martin Stingl <martin.stingl@dfv.de>
2017-02-22 09:52:01 +0100

    Don't store data as factor, store numerical data as numeric

--- R/speed-insights-package.R
+++ R/speed-insights-package.R
@@ -15,7 +15,9 @@ library(gtools)
 #' @export
 speedfinder <- function(url,strategy,key) {
   pid <- RJSONIO::fromJSON(paste0("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=",url,"&strategy=",strategy,"&key=",key))
-  frame1 <- cbind(as.data.frame(pid[2]),as.data.frame(pid[3]),as.data.frame(pid[5]),as.data.frame(pid[6]))
+  tmp <- as.data.frame(pid[6], stringsAsFactors=FALSE)
+  tmp <- as.data.frame(lapply(tmp, as.numeric), stringsAsFactors=FALSE)
+  frame1 <- cbind(as.data.frame(pid[2], stringsAsFactors=FALSE),as.data.frame(pid[3], stringsAsFactors=FALSE),as.data.frame(pid[5], stringsAsFactors=FALSE), tmp)
   rbind.data.frame(data.frame(), frame1,make.row.names=FALSE)
 }
 
@@ -27,7 +29,9 @@ speedfinder <- function(url,strategy,key) {
 #' @export
 speedfinder2 <- function(url,strategy,key) {
   pid <- RJSONIO::fromJSON(paste0("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=",url,"&strategy=",strategy,"&key=",key))
-  frame1 <- cbind(as.data.frame(pid[2]),as.data.frame(pid[3]),as.data.frame(pid[5]),as.data.frame(pid[6]))
+  tmp <- as.data.frame(pid[6], stringsAsFactors=FALSE)
+  tmp <- as.data.frame(lapply(tmp, as.numeric), stringsAsFactors=FALSE)
+  frame1 <- cbind(as.data.frame(pid[2], stringsAsFactors=FALSE),as.data.frame(pid[3], stringsAsFactors=FALSE),as.data.frame(pid[5], stringsAsFactors=FALSE), tmp)
 }
 
 #' Speed results for a list of URLs
